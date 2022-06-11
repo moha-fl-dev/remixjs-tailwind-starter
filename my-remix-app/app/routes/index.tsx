3e44a0b7ef9b29f6713400c49379429a9803abbb
moha-fl-dev <moha_ali@live.nl>
2022-06-11 08:31:36 +0200

    first commit

---
+++ my-remix-app/app/routes/index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-50 p-5">
+        
+        Welcome to Remix
+        
+      </h1>
+    </div>
+  );
+}
