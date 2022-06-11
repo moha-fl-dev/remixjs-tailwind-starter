a7d6bd2c333af630029a786ba62e250be4f617c0
moha-fl-dev <moha_ali@live.nl>
2022-06-11 09:18:37 +0200

    initial commit for new-message

--- my-remix-app/app/routes/index.tsx
+++ my-remix-app/app/routes/index.tsx
@@ -3,8 +3,8 @@ export default function Index() {
     <div>
       <h1 className="text-3xl font-bold underline bg-red-50 p-5">
         
-        Welcome to Remix
-        
+        Welcome to Remix Get started
+
       </h1>
     </div>
   );
